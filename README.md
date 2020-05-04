b1b4aad951f6dee92378661386c994577d922e1c
devirakshith <64076282+devirakshith@users.noreply.github.com>
2020-05-04 13:35:32 +0000

    test

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
-# angular-devi-assignment-3
+﻿# angular-devi-assignment-3
+
+# Add Bootstrap3 for proper view
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-devi-assignment-3)
---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+
+const routes: Routes = [
+  {path: 'home', loadChildren: () => import('./home/home.module').then(m => m.HomeModule)},
+  {path: 'contact', loadChildren: () => import('./contact/contact.module').then(m => m.ContactModule)},
+  {path: 'about', loadChildren: () => import('./about/about.module').then(m => m.AboutModule)}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
